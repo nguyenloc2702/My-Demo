1567256afcc319d93028637a549b4d58707d2101
Nguyen Loc <nguyenloc@vnpt.vn>
2025-10-22 13:20:31 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,44 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
+import * as path from "node:path";
 
 export default defineConfig({
     plugins: [react()],
+    // build: {
+    //     lib: {
+    //         entry: 'src/index.ts',
+    //         name: 'MyDemo',
+    //         fileName: () => `my-demo.umd.js`,
+    //         formats: ['umd'],
+    //     },
+    //     rollupOptions: {
+    //         external: ['react', 'react-dom'],
+    //         output: {
+    //             globals: {
+    //                 react: 'React',
+    //                 'react-dom': 'ReactDOM',
+    //             },
+    //         },
+    //     },
+    //     sourcemap: false,
+    //     minify: true,
+    // },
     build: {
         lib: {
-            entry: 'src/index.ts',
-            name: 'MyDemo',
-            fileName: () => `my-demo.umd.js`,
+            entry: path.resolve(__dirname, 'src/index.ts'),
+            name: 'MFECore',
+            fileName: () => 'my-demo.umd.js',
             formats: ['umd'],
         },
+        outDir: 'dist-umd',
         rollupOptions: {
             external: ['react', 'react-dom'],
             output: {
                 globals: {
                     react: 'React',
                     'react-dom': 'ReactDOM',
-                },
-            },
+                }
+            }
         },
         sourcemap: false,
         minify: true,
