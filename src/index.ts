4b21b021c88b20f6249064780cbc79ffd51d2fc1
Nguyen Loc <nguyenloc@vnpt.vn>
2025-10-22 13:37:26 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 
-import DataGrid, {Column, ColumnFixing} from "devextreme-react/data-grid";
-
 import {DataGridComponent} from "./DataGridComponent/DataGridComponent.tsx";
 
-export {React, ReactDOM, DataGridComponent, DataGrid, Column, ColumnFixing};
+export {React, ReactDOM, DataGridComponent};
