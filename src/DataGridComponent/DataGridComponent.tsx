af5bebe4cadc9d24e842e42a4239cfcebda5ecec
Nguyen Loc <nguyenloc@vnpt.vn>
2025-10-22 14:07:06 +0700

    update ver 0.3

--- src/DataGridComponent/DataGridComponent.tsx
+++ src/DataGridComponent/DataGridComponent.tsx
@@ -1,5 +1,48 @@
+import DataGrid, {Column, ColumnFixing} from "devextreme-react/data-grid";
+
+import {type Employee, employees} from './data.ts';
+
+const calculateCellValue = (data: Employee) => [data.Title, data.FirstName, data.LastName].join(' ');
+
+
 export const DataGridComponent = () => {
     return (
-        <div>test from React</div>
+        <DataGrid
+            id="gridContainer"
+            dataSource={employees}
+            keyExpr="ID"
+            allowColumnReordering={true}
+            columnAutoWidth={true}
+            showBorders={true}
+        >
+            <ColumnFixing enabled={true}/>
+            <Column
+                caption="Employee"
+                fixed={true}
+                calculateCellValue={calculateCellValue}
+            />
+            <Column
+                dataField="Position"
+                alignment="right"
+                fixed={true}
+            />
+            <Column dataField="Address"/>
+            <Column dataField="City"/>
+            <Column dataField="Zipcode"/>
+            <Column dataField="State"/>
+            <Column dataField="Department"/>
+            <Column
+                dataField="BirthDate"
+                dataType="date"
+            />
+            <Column
+                dataField="HireDate"
+                dataType="date"
+            />
+            <Column dataField="HomePhone"/>
+            <Column dataField="MobilePhone"/>
+            <Column dataField="Email"/>
+            <Column dataField="Skype"/>
+        </DataGrid>
     );
 };
