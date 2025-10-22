e044be8ace673b6b191dbe3f9bd51d8bca2adf2a
Nguyen Loc <nguyenloc@vnpt.vn>
2025-10-22 13:46:22 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,8 @@ import {DataGridComponent} from "./DataGridComponent/DataGridComponent.tsx";
 
 export {React, ReactDOM, DataGridComponent};
 
-declare global {
-    interface Window {
-        MFECore: {
-            DataGridComponent: typeof DataGridComponent;
-        };
-    }
-}
-
-if (typeof window !== "undefined") {
-    window.MFECore = {
-        DataGridComponent
-    };
-}
+// if (typeof window !== "undefined") {
+//     window.MFECore = {
+//         DataGridComponent
+//     };
+// }
--- vite.config.ts
+++ vite.config.ts
@@ -4,30 +4,11 @@ import * as path from "node:path";
 
 export default defineConfig({
     plugins: [react()],
-    // build: {
-    //     lib: {
-    //         entry: 'src/index.ts',
-    //         name: 'MyDemo',
-    //         fileName: () => `my-demo.umd.js`,
-    //         formats: ['umd'],
-    //     },
-    //     rollupOptions: {
-    //         external: ['react', 'react-dom'],
-    //         output: {
-    //             globals: {
-    //                 react: 'React',
-    //                 'react-dom': 'ReactDOM',
-    //             },
-    //         },
-    //     },
-    //     sourcemap: false,
-    //     minify: true,
-    // },
     build: {
         lib: {
             entry: path.resolve(__dirname, 'src/index.ts'),
             name: 'MFECore',
-            fileName: () => 'my-demo.umd.js',
+            fileName: () => 'mfe-core.umd.js',
             formats: ['umd'],
         },
         outDir: 'dist-umd',
