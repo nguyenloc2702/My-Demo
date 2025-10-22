90b1421918445fec7b2bdf5b7b203aa0da6897b5
Nguyen Loc <nguyenloc@vnpt.vn>
2025-10-22 13:54:14 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom/client';
-
 import {DataGridComponent} from "./DataGridComponent/DataGridComponent.tsx";
 
-export {React, ReactDOM, DataGridComponent};
+export {DataGridComponent};
 
-// if (typeof window !== "undefined") {
-//     window.MFECore = {
-//         DataGridComponent
-//     };
-// }
+declare global {
+    interface Window {
+        MFECore: {
+            DataGridComponent: typeof DataGridComponent;
+        };
+    }
+}
+
+if (typeof window !== 'undefined') {
+    window.MFECore = {
+        DataGridComponent
+    };
+}
